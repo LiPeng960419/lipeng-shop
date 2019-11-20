492b5e0acce5962170e4f9d12c2f5337277f7ff3
lipeng <lp960419@163.com>
2019-11-20 14:41:11 +0800

    提交

--- service/pay/src/main/java/com/lipeng/pay/callback/servjce/PayAsynCallbackService.java
+++ service/pay/src/main/java/com/lipeng/pay/callback/servjce/PayAsynCallbackService.java
@@ -5,10 +5,12 @@ import com.lipeng.pay.callback.template.factory.TemplateFactory;
 import com.lipeng.pay.strategy.PayStrategy;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class PayAsynCallbackService {
 
     private static final String UNIONPAYCALLBACK_TEMPLATE = "unionPayCallbackTemplate";
@@ -22,7 +24,12 @@ public class PayAsynCallbackService {
     public String unionPayAsynCallback(HttpServletRequest req, HttpServletResponse resp) {
         AbstractPayCallbackTemplate abstractPayCallbackTemplate = TemplateFactory
                 .getPayCallbackTemplate(UNIONPAYCALLBACK_TEMPLATE);
-        return abstractPayCallbackTemplate.asyncCallBack(req, resp, PayStrategy.UNION_PAY_CHANNEL_ID);
+        try {
+            return abstractPayCallbackTemplate.asyncCallBack(req, resp, PayStrategy.UNION_PAY_CHANNEL_ID);
+        } catch (Exception e) {
+            log.error("unionPayAsynCallback Exception", e);
+        }
+        return null;
     }
 
     /**
@@ -32,7 +39,12 @@ public class PayAsynCallbackService {
     public String aliPayAsynCallback(HttpServletRequest req, HttpServletResponse resp) {
         AbstractPayCallbackTemplate abstractPayCallbackTemplate = TemplateFactory
                 .getPayCallbackTemplate(ALIPAYCALLBACK_TEMPLATE);
-        return abstractPayCallbackTemplate.asyncCallBack(req, resp, PayStrategy.ALI_PAY_CHANNEL_ID);
+        try {
+            return abstractPayCallbackTemplate.asyncCallBack(req, resp, PayStrategy.ALI_PAY_CHANNEL_ID);
+        } catch (Exception e) {
+            log.error("aliPayAsynCallback Exception", e);
+        }
+        return null;
     }
 
 }
