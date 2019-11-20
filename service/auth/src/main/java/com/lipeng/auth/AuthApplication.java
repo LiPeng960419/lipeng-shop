87cab32a30df93ae218bd30664d038a997e5319f
lipeng <lp960419@163.com>
2019-11-20 17:44:18 +0800

    提交

---
+++ service/auth/src/main/java/com/lipeng/auth/AuthApplication.java
@@ -0,0 +1,19 @@
+package com.lipeng.auth;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@MapperScan(basePackages = "com.lipeng.auth.mapper")
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
