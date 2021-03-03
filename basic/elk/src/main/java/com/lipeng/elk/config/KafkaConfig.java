41106a1fbc5a2b22f6d9f77190f0019ef00d815a
lipeng <lp960419@163.com>
2021-03-03 17:26:00 +0800

    commit

--- basic/elk/src/main/java/com/lipeng/elk/config/KafkaConfig.java
+++ basic/elk/src/main/java/com/lipeng/elk/config/KafkaConfig.java
@@ -2,15 +2,25 @@ package com.lipeng.elk.config;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Listener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.AbstractMessageListenerContainer.AckMode;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 
 /**
  * @Author: lipeng 910138
@@ -54,6 +64,43 @@ public class KafkaConfig {
     @Bean
     KafkaTemplate kafkaTemplate() {
         return new KafkaTemplate(producerFactory());
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setConcurrency(10);
+        factory.getContainerProperties().setPollTimeout(1500);
+        //批量消费
+        factory.setBatchListener(true);
+        factory.getContainerProperties().setAckMode(AckMode.BATCH);
+        return factory;
+    }
+
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> propsMap = new HashMap<>();
+        propsMap.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+        propsMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        propsMap.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        propsMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        propsMap.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        propsMap.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 100);
+        return propsMap;
+    }
+
+    @Bean
+    public Listener listener() {
+        return new Listener();
+    }
+
+    @KafkaListener(groupId = "lipeng-shop-elk-log-group", topics = "lipeng-shop-elk-log-topic")
+    public void listener1(String record) {
+        System.out.println(("listener1 Receive： +++++++++++++++ Record:" + record));
     }
 
 }
