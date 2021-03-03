96a8bb1389e36d7098d366d00162e1c80d972b27
lipeng <lp960419@163.com>
2021-03-03 17:13:49 +0800

    commit

--- service/member/sql/meite_member.sql
+++ service/member/sql/meite_member.sql
@@ -1,77 +1,82 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : 127.0.0.1
-Source Server Version : 50715
-Source Host           : 127.0.0.1:3306
-Source Database       : meite_member
+ Source Server         : 127.0.0.1
+ Source Server Type    : MySQL
+ Source Server Version : 50727
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : meite_member
 
-Target Server Type    : MYSQL
-Target Server Version : 50715
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50727
+ File Encoding         : 65001
 
-Date: 2019-11-14 21:14:55
+ Date: 03/03/2021 17:13:07
 */
 
+SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for `meite_user`
+-- Table structure for meite_user
 -- ----------------------------
 DROP TABLE IF EXISTS `meite_user`;
 CREATE TABLE `meite_user`
 (
-  `USER_ID`     bigint(20) NOT NULL AUTO_INCREMENT,
-  `MOBILE`      varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `EMAIL`       varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `PASSWORD`    varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `USER_NAME`   varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `SEX`         tinyint(1)                       DEFAULT NULL,
-  `AGE`         smallint(3)                      DEFAULT NULL,
-  `CREATE_TIME` datetime                         DEFAULT NULL,
-  `UPDATE_TIME` datetime                         DEFAULT NULL,
-  `IS_AVALIBLE` tinyint(1)                       DEFAULT NULL,
-  `PIC_IMG`     varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `QQ_OPENID`   varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `WX_OPENID`   varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `WB_OPENID`   varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY (`USER_ID`)
+    `USER_ID`     bigint(20)                                             NOT NULL AUTO_INCREMENT,
+    `MOBILE`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+    `EMAIL`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+    `PASSWORD`    varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+    `USER_NAME`   varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+    `SEX`         tinyint(1)                                             NULL DEFAULT NULL,
+    `AGE`         smallint(3)                                            NULL DEFAULT NULL,
+    `CREATE_TIME` datetime(0)                                            NULL DEFAULT NULL,
+    `UPDATE_TIME` datetime(0)                                            NULL DEFAULT NULL,
+    `IS_AVALIBLE` tinyint(1)                                             NULL DEFAULT NULL,
+    `PIC_IMG`     varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+    `QQ_OPENID`   varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+    `WX_OPENID`   varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+    `WB_OPENID`   varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+    PRIMARY KEY (`USER_ID`) USING BTREE
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 2
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_bin;
+  AUTO_INCREMENT = 3
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_bin
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of meite_user
 -- ----------------------------
 INSERT INTO `meite_user`
-VALUES ('1', '15671564665', null, 'E10ADC3949BA59ABBE56E057F20F883E', 'Insomnia', '0', null,
-        '2019-11-11 09:36:06', '2019-11-12 09:41:16', '1', null, null,
-        'o1eIhxH9G8AdOSEQuyRai1LkEceo');
+VALUES (2, '15671564665', NULL, '96E79218965EB72C92A549DD5A330112', NULL, NULL, NULL,
+        '2021-03-03 16:51:38', NULL, 1, NULL, NULL, NULL, NULL);
 
 -- ----------------------------
--- Table structure for `meite_user_token`
+-- Table structure for meite_user_token
 -- ----------------------------
 DROP TABLE IF EXISTS `meite_user_token`;
 CREATE TABLE `meite_user_token`
 (
-  `id`              bigint(20) NOT NULL AUTO_INCREMENT,
-  `token`           varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `login_type`      varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `device_infor`    varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  `is_availability` tinyint(1)                       DEFAULT NULL,
-  `user_id`         bigint(20) NOT NULL,
-  `create_time`     datetime                         DEFAULT NULL,
-  `update_time`     datetime                         DEFAULT NULL,
-  PRIMARY KEY (`id`)
+    `id`              bigint(20)                                             NOT NULL AUTO_INCREMENT,
+    `token`           varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+    `login_type`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+    `device_infor`    varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+    `is_availability` tinyint(1)                                             NULL DEFAULT NULL,
+    `user_id`         bigint(20)                                             NOT NULL,
+    `create_time`     datetime(0)                                            NULL DEFAULT NULL,
+    `update_time`     datetime(0)                                            NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 2
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_bin;
+  AUTO_INCREMENT = 3
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_bin
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of meite_user_token
 -- ----------------------------
 INSERT INTO `meite_user_token`
-VALUES ('1', 'mt.mb.loginPCe69d7f308c27489dbc6314da54dbe388', 'PC', 'Chrome/75.0.3770.100', '0',
-        '1', '2019-11-12 09:19:49', '2019-11-12 21:00:44');
+VALUES (2, 'mt.mb.loginPCd7a8135c0d684d5d9615ed2bb99d0133', 'PC', 'Chrome 8/87.0.4280.66', 0, 2,
+        '2021-03-03 16:52:29', NULL);
+
+SET FOREIGN_KEY_CHECKS = 1;
